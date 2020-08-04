556bba50ba82f649d28cc4437c261dbffa4233b0
namacha <mac.ayu15@gmail.com>
2020-08-04 14:18:38 +0900

    fix: fixed shell script

--- symlink.sh
+++ symlink.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 mkdir $HOME/.vim-backup
 cd `dirname $0`
+
 for dotfiles in .?*
 do
-    if [ $dotfile != '..' ] && [ $dotfile != '.git' ]
+    if [ "$dotfile" != ".." ] && [ "$dotfile" != ".git" ];
     then
         ln -Fis "$PWD/$dotfiles" $HOME
     fi
